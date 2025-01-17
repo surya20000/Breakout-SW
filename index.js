80c561a2adde0190e96340260225dbe77062bdcc
surya20000 <surya.p@kalvium.community>
2025-01-17 13:59:39 +0530

    Initial game phase completed

---
+++ index.js
@@ -0,0 +1,45 @@
+const grid = document.querySelector(".grid");
+const scoreDisplay = document.querySelector("#score");
+const blockWidth = 100;
+const blockHeight = 20;
+const ballDiameter = 20;
+const boardWidth = 560;
+const boardHeight = 300;
+let xDirection = -2;
+let yDirection = 2;
+
+const userStart = [230, 10];
+let currentPosition = userStart;
+
+const ballStart = [270, 40];
+let ballCurrentPosition = ballStart;
+
+let timerId;
+let score = 0;
+
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+  }
+}
+
+const blocks = [
+  new Block(10, 270),
+  new Block(120, 270),
+  new Block(230, 270),
+  new Block(340, 270),
+  new Block(450, 270),
+  new Block(10, 240),
+  new Block(120, 240),
+  new Block(230, 240),
+  new Block(340, 240),
+  new Block(450, 240),
+  new Block(10, 210),
+  new Block(120, 210),
+  new Block(230, 210),
+  new Block(340, 210),
+  new Block(450, 210),
+];
